22a18f15e0cdb8196749d65dca7a72b2333398b3
Kailas <kailasdoke100@gmail.com>
2026-05-11 08:29:43 +0530

    This is the Spring Web MVC program

--- MevanWebApp/src/main/java/in/kd/main/MyController.java
+++ MevanWebApp/src/main/java/in/kd/main/MyController.java
@@ -1,5 +1,21 @@
-package in.kd.main;
+ package in.kd.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller 
 public class MyController {
+	
+	@GetMapping("/hellopahe")
+	public ModelAndView openhellopage() {
+		
+		System.out.println("openhellopage");
+		
+		ModelAndView mvc = new ModelAndView();
+		mvc.setViewName("hello");
+		
+		return mvc;
+	}
 
 }
--- MevanWebApp/src/main/webapp/index.jsp
+++ MevanWebApp/src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+
+<a href="hellopahe">open Hello page</a>
 </body>
 </html>
